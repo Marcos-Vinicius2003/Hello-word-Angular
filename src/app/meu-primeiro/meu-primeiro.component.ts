9bf02f62185df6bed712412798b0219d6cb902a3
Marcos Vinicius <marcossilvareis.vinicios3@gmail.com>
2020-03-16 10:22:50 -0300

    16/03

---
+++ src/app/meu-primeiro/meu-primeiro.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { from } from 'rxjs';
+
+@Component({
+    selector: 'meu-primeiro-componet',
+    template: `
+    <p> Meu primeiro component com Angular 2! </p>
+    `
+})
+export class MeuPrimeiroComponent { }
